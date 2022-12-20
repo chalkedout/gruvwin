b2ab28de15b247e0ebd2f1432b3ae7c57fbb5621
chalkedout <120605557+chalkedout@users.noreply.github.com>
2022-12-20 05:43:43 +0530

    Add files via upload

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/patriksvensson.omp.json" | Invoke-Expression
+Set-Alias fetch winfetch
+Import-Module posh-git
+Start-Process -FilePath "C:\Program Files\Rainmeter\Rainmeter.exe"
+Start-Process -FilePath "C:\Program Files\Rainmeter\GlazeWM_x64_1.7.exe"
+clear
